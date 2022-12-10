7def42c78765d1e9971f23e912a12e976bcb8794
thesafatem <thesafatem@gmail.com>
2022-12-11 03:15:29 +0600

    Describe first models

---
+++ src/top-page/top-page.model.ts
@@ -0,0 +1,26 @@
+export enum TopCategory {
+	Courses,
+	Services,
+	Books,
+	Goods
+}
+
+export class TopPageModel {
+	firstCategory: TopCategory;
+	secondCategory: string;
+	title: string;
+	category: string;
+	hh?: {
+		count: number;
+		juniorSalary: number;
+		middleSalary: number;
+		seniorSalary: number;
+	};
+	advantages: {
+		title: string;
+		description: string;
+	}[];
+	seoText: string;
+	tagsTitle: string;
+	tags: string[];
+}
