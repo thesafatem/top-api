8c7ef3e1976f3fb56d770832e05dc9bc94565e4b
thesafatem <thesafatem@gmail.com>
2023-01-21 05:53:17 +0600

    Add review service logic

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 import { prop } from '@typegoose/typegoose/lib/prop';
+import { Types } from 'mongoose';
 
 export interface ReviewModel extends Base { }
 
@@ -15,4 +16,7 @@ export class ReviewModel extends TimeStamps {
 
 	@prop()
 	rating: number;
+
+	@prop()
+	productId: Types.ObjectId
 }
