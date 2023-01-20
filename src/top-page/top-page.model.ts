490aa9bf6275ed0f9d17c2496292ecf772f07445
thesafatem <thesafatem@gmail.com>
2023-01-20 06:20:01 +0600

    Describe mongo in top page module

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,26 +1,64 @@
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose/lib/prop';
+
 export enum TopCategory {
-  Courses,
-  Services,
-  Books,
-  Goods,
+	Courses,
+	Services,
+	Books,
+	Goods,
 }
 
-export class TopPageModel {
-  firstCategory: TopCategory;
-  secondCategory: string;
-  title: string;
-  category: string;
-  hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
-  seoText: string;
-  tagsTitle: string;
-  tags: string[];
+export class HhData {
+	@prop()
+	count: number;
+
+	@prop()
+	juniorSalary: number;
+
+	@prop()
+	middleSalary: number;
+
+	@prop()
+	seniorSalary: number;
+}
+
+export class TopPageAdvantage {
+	@prop()
+	title: string;
+
+	@prop()
+	description: string;
+}
+
+export interface TopPageModel extends Base { }
+export class TopPageModel extends TimeStamps {
+	@prop({ enum: TopCategory })
+	firstCategory: TopCategory;
+
+	@prop()
+	secondCategory: string;
+
+	@prop({ uniqie: true })
+	alias: string;
+
+	@prop()
+	title: string;
+
+	@prop()
+	category: string;
+
+	@prop({ type: () => HhData })
+	hh?: HhData;
+
+	@prop({ type: () => [TopPageAdvantage] })
+	advantages: TopPageAdvantage[];
+
+	@prop()
+	seoText: string;
+
+	@prop()
+	tagsTitle: string;
+
+	@prop({ type: () => [String] })
+	tags: string[];
 }
