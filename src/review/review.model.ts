b97dbbcced95481af783a946ec5571d59e438095
thesafatem <thesafatem@gmail.com>
2023-01-20 06:05:04 +0600

    Describe mongo in review module

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,18 @@
-export class ReviewModel {
-  _id: string;
-  name: string;
-  title: string;
-  description: string;
-  rating: number;
-  createdAt: Date;
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose/lib/prop';
+
+export interface ReviewModel extends Base { }
+
+export class ReviewModel extends TimeStamps {
+	@prop()
+	name: string;
+
+	@prop()
+	title: string;
+
+	@prop()
+	description: string;
+
+	@prop()
+	rating: number;
 }
