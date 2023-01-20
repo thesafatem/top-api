d4bf1280c1498b83ce25111b69554e192e963b78
thesafatem <thesafatem@gmail.com>
2023-01-20 06:02:20 +0600

    Describe mongo in product module

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,53 @@
-export class ProductModel {
-  _id: string;
-  image: string;
-  title: string;
-  price: number;
-  oldPrice: number;
-  credit: number;
-  calculatedRating: number;
-  description: string;
-  advantages: string;
-  disadvantages: string;
-  categories: string[];
-  tags: string[];
-  characteristics: {
-    [key: string]: string;
-  };
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose/lib/prop';
+
+class ProductCharacteristic {
+	@prop()
+	name: string;
+
+	@prop()
+	value: string;
+}
+
+// Base has _id, _v, _t fields
+export interface ProductModel extends Base { }
+
+// TimeStamps has createdAt, updatedAt fields
+export class ProductModel extends TimeStamps {
+	@prop()
+	image: string;
+
+	@prop()
+	title: string;
+
+	@prop()
+	price: number;
+
+	@prop()
+	oldPrice: number;
+
+	@prop()
+	credit: number;
+
+	@prop()
+	calculatedRating: number;
+
+	@prop()
+	description: string;
+
+	@prop()
+	advantages: string;
+
+	@prop()
+	disadvantages: string;
+
+	@prop({ type: () => [String] })
+	categories: string[];
+
+	@prop({ type: () => [String] })
+	tags: string[];
+
+	// id: false to not create index for nested keys
+	@prop({ type: () => [ProductCharacteristic], _id: false })
+	characteristics: ProductCharacteristic[];
 }
