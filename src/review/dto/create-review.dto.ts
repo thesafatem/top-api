536e5b18be91903a1d17fc79a06e986fe95de72e
thesafatem <thesafatem@gmail.com>
2023-01-24 04:54:48 +0600

    Add validation pipe for create review dto

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,22 @@
+import { IsString, IsNumber, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+  // Decorators for validation of values from body
+  @IsString()
   name: string;
+
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  // Customize validation error message
+  @Min(1, { message: 'Rating must not be less than 1' })
+  @Max(5)
+  @IsNumber()
   rating: number;
+
+  @IsString()
   productId: string;
 }
