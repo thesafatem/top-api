d13af8e86d7689a86b3c046df01c4746e1751325
thesafatem <thesafatem@gmail.com>
2023-02-03 03:28:38 +0600

    Add custom decorator to extract user from request

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,17 +1,18 @@
 import {
-  Controller,
-  Body,
-  Param,
-  Get,
-  Post,
-  Patch,
-  Delete,
-  HttpCode,
-  HttpException,
-  HttpStatus,
+	Controller,
+	Body,
+	Param,
+	Get,
+	Post,
+	Patch,
+	Delete,
+	HttpCode,
+	HttpException,
+	HttpStatus,
 } from '@nestjs/common';
 import { UseGuards } from '@nestjs/common/decorators/core/use-guards.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 import { CreateProductDto } from './dto/create-product.dto';
 import { FindProductDto } from './dto/find-product.dto';
 import { PRODUCT_NOT_FOUND } from './product.constants';
@@ -20,38 +21,38 @@ import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+	constructor(private readonly productService: ProductService) { }
 
-  @UseGuards(JwtAuthGuard)
-  @Post('create')
-  async create(@Body() dto: CreateProductDto) {
-    return this.productService.create(dto);
-  }
+	@UseGuards(JwtAuthGuard)
+	@Post('create')
+	async create(@Body() dto: CreateProductDto, @UserEmail() email: string) {
+		return this.productService.create(dto);
+	}
 
-  @Get(':id')
-  async get(@Param('id') id: string) {
-    const product = this.productService.getById(id);
+	@Get(':id')
+	async get(@Param('id') id: string) {
+		const product = this.productService.getById(id);
 
-    if (!product) {
-      throw new HttpException(PRODUCT_NOT_FOUND, HttpStatus.NOT_FOUND);
-    }
+		if (!product) {
+			throw new HttpException(PRODUCT_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
 
-    return product;
-  }
+		return product;
+	}
 
-  @Delete(':id')
-  async delete(@Param('id') id: string) {
-    const deletedDoc = await this.productService.deleteById(id);
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		const deletedDoc = await this.productService.deleteById(id);
 
-    if (!deletedDoc) {
-      throw new HttpException(PRODUCT_NOT_FOUND, HttpStatus.NOT_FOUND);
-    }
-  }
+		if (!deletedDoc) {
+			throw new HttpException(PRODUCT_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
+	}
 
-  //   @Patch(':id')
-  //   async patch(@Param('id') id: string, @Body() dto: ProductModel) {}
+	//   @Patch(':id')
+	//   async patch(@Param('id') id: string, @Body() dto: ProductModel) {}
 
-  //   @HttpCode(200)
-  //   @Post()
-  //   async find(@Body() dto: FindProductDto) {}
+	//   @HttpCode(200)
+	//   @Post()
+	//   async find(@Body() dto: FindProductDto) {}
 }
