0489a781ca5ebbe9c9ad9ca193c9b7c74c771aec
thesafatem <thesafatem@gmail.com>
2023-03-07 03:56:22 +0600

    Add file upload logic

---
+++ src/files/files.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, HttpCode, Post, UseInterceptors } from '@nestjs/common';
+import { UploadedFile, UseGuards } from '@nestjs/common/decorators';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { FileElementResponse } from './dto/file-element.response';
+import { FilesService } from './files.service';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('upload')
+  @HttpCode(200)
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('files'))
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<FileElementResponse[]> {
+    return this.filesService.saveFiles([file]);
+  }
+}
