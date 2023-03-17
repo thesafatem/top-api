5f080313e1de4b002385e4e9ea1f40ec44fdb239
thesafatem <thesafatem@gmail.com>
2023-03-17 18:38:31 +0600

    Add telegram module with configs and dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { TypegooseModule } from 'nestjs-typegoose';
 import { getMongoConfig } from './configs/mongo.config';
 import { FilesModule } from './files/files.module';
 import { SitemapModule } from './sitemap/sitemap.module';
+import { TelegramModule } from './telegram/telegram.module';
+import { getTelegramConfig } from './configs/telegram.config';
 
 @Module({
   imports: [
@@ -23,6 +25,11 @@ import { SitemapModule } from './sitemap/sitemap.module';
     ReviewModule,
     FilesModule,
     SitemapModule,
+    TelegramModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getTelegramConfig,
+    }),
   ],
 })
 export class AppModule {}
