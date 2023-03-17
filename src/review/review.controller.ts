09488679471b839ae61c73b361e767bf7f9505a9
thesafatem <thesafatem@gmail.com>
2023-03-17 18:39:07 +0600

    Add review route to send telegram message

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -17,16 +17,33 @@ import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
+import { TelegramService } from 'src/telegram/telegram.service';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(
+    private readonly reviewService: ReviewService,
+    private readonly telegramService: TelegramService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @UsePipes(new ValidationPipe())
   @Post('/')
   async create(@Body() dto: CreateReviewDto) {
     return this.reviewService.create(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(new ValidationPipe())
+  @Post('notify')
+  async notify(@Body() dto: CreateReviewDto) {
+    const message =
+      `Name: ${dto.name}\n` +
+      `Heading: ${dto.title}\n` +
+      `Description: ${dto.description}\n` +
+      `Rating: ${dto.rating}\n` +
+      `Product ID: ${dto.productId}\n`;
+    return this.telegramService.sendMessage(message);
   }
 
   @Delete(':id')
